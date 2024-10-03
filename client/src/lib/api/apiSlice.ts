6a14a76593f44281ee08d0bbce67a855685dc664
Roy Sheppard <rsheppard83@gmail.com>
2024-10-03 22:44:32 +0100

    Refactor API base query to include authorization header

--- client/src/lib/api/apiSlice.ts
+++ client/src/lib/api/apiSlice.ts
@@ -1,9 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession } from "aws-amplify/auth";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { accessToken } = session.tokens ?? {};
+
+      if (accessToken) {
+        headers.set("Authorization", `Bearer ${accessToken}`);
+      }
+      return headers;
+    },
+  }),
   tagTypes: ["Projects", "Tasks", "Users"],
   endpoints: () => ({}),
 });
